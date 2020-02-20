6567eb7268d0be4565e2675e82c800ffdcb0fede
xeusteerapat <xeus085@gmail.com>
2020-02-20 23:56:51 +0700

    refactor findTwoMaxNums

--- findTwoMaxNums.js
+++ findTwoMaxNums.js
@@ -1,15 +1,15 @@
-let max = null;
-let min = null;
+let max = -Infinity;
+let secondMax = -Infinity;
 let a;
 
 while (a !== null) {
   a = parseInt(prompt("Enter number"));
-  if (a > max) {
-    min = max;
+  if (a >= max) {
+    secondMax = max;
     max = a;
-  } else if (a > min && a !== max) {
-    min = a;
+  } else if (a > secondMax && a !== max) {
+    secondMax = a;
   } else if (!a) break;
 }
 
-alert(`Max num is ${max} and min is ${min}`);
+alert(`Max num is ${max} and secondMax is ${secondMax}`);
